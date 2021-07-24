6ab4d19c2ad71deb3092968b15886fcaecb9e1ba
akiyama-prog <autumn56y@gmail.com>
2021-07-24 18:04:57 +0900

    back end skills

--- src/Skill.tsx
+++ src/Skill.tsx
@@ -6,6 +6,12 @@ import js from './images/javascript.png';
 import vue from './images/vue.png';
 import react from './images/react.png';
 import wp from './images/wp.jpeg';
+import php from './images/php.png';
+import laravel from './images/laravel.png';
+import github from './images/github.png';
+import psql from './images/postgres.png';
+import mysql from './images/MySQL.png';
+import docker from './images/docker.jpeg';
 
 function Skill() {
 return (
@@ -13,7 +19,7 @@ return (
     <h2>SKILL</h2>
     <div className="wrap">
         <div className="front">
-            <div className="item">
+            <div className="item html">
                 <img src={html} alt="html5" />
                 <p className="skill-title html">HTML5<br />★★★★☆</p>
             </div>
@@ -26,12 +32,12 @@ return (
                 <p className="skill-title">JavaScript/<br />jquery<br />★★★☆☆</p>
             </div>
             <div className="item">
-                <img src={vue} alt="vue" />
-                <p className="skill-title">Vue.js<br />★★☆☆☆</p>
-            </div>
-            <div className="item">
                 <img src={react} alt="react" />
                 <p className="skill-title">React.js<br />★★★☆☆</p>
+            </div>
+            <div className="item">
+                <img src={vue} alt="vue" />
+                <p className="skill-title">Vue.js<br />★★☆☆☆</p>
             </div>
             <div className="item">
                 <img src={wp} alt="wordpress" />
@@ -40,20 +46,28 @@ return (
         </div>
         <div className="back">
             <div className="item">
-                <img src={html} alt="html5" />
-                <p className="skill-title">HTML5<br />★★★★☆</p>
+                <img src={php} alt="php" />
+                <p className="skill-title">PHP<br />★★★★☆</p>
             </div>
             <div className="item">
-                <img src={html} alt="html5" />
-                <p className="skill-title">HTML5<br />★★★★☆</p>
+                <img src={laravel} alt="laravel" />
+                <p className="skill-title">Laravel<br />★★★★☆</p>
             </div>
             <div className="item">
-                <img src={html} alt="html5" />
-                <p className="skill-title">HTML5<br />★★★★☆</p>
+                <img src={github} alt="github" />
+                <p className="skill-title">Github<br />★★★★☆</p>
             </div>
             <div className="item">
-                <img src={html} alt="html5" />
-                <p className="skill-title">HTML5<br />★★★★☆</p>
+                <img src={psql} alt="postgres" />
+                <p className="skill-title">PostgreSQL<br />★★★★☆</p>
+            </div>
+            <div className="item">
+                <img src={mysql} alt="mysql" />
+                <p className="skill-title">MySQL<br />★★☆☆☆</p>
+            </div>
+            <div className="item">
+                <img src={docker} alt="docker" />
+                <p className="skill-title">Docker<br />★★☆☆☆</p>
             </div>
         </div>
     </div>
