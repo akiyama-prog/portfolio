0e220c79e32d41b1ced207903f350d9631f85eca
akiyama-prog <autumn56y@gmail.com>
2021-11-23 23:10:34 +0900

    workの英語説明コンポーネント作成

--- src/Works.tsx
+++ src/Works.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { render } from "react-dom";
 import {
     BrowserRouter,
+    Routes,
     Route
 } from "react-router-dom";
 import tokyoshare from './images/tokyoshare.png';
@@ -13,6 +14,34 @@ import isara from './images/isara.png';
 import isara_png from './images/isara_practice.png';
 import aklog from './images/aklog.net.png';
 
+const EnDentalDescribe = () => {
+    return <>Dental Office web site</>;
+};
+
+const EnShareHouseDescribe = () => {
+    return <>Dental Office web site</>;
+};
+
+const EnUnitPriceCalcDescribe = () => {
+    return <>Unit price calculator</>;
+};
+
+const EnIsaraDescribe = () => {
+    return (
+        <>
+            iSara LP practice(<a href="https://isara.life/" target="_blank">original web site</a>)
+        </>
+    );
+};
+
+const EnBlogDescribe = () => {
+    return <>My blog</>;
+};
+
+const EnPortfolioDescribe = () => {
+    return <>My portfolio</>;
+};
+
 function Works() {
     return (
         <div id="works">
@@ -20,27 +49,63 @@ function Works() {
             <div className="my-works">
                 <div className="work-img">
                     <a href="https://minami-dental-office.vercel.app/" target="_blank"><img src={dental} alt="dentalsite" /></a>
-                    <p className="title">Dental Office web site<br /><span className="skill-explaine">Next.js / SCSS / StaticForm / Docker</span></p>
+                    <p className="title">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="en" element={<EnDentalDescribe />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <br /><span className="skill-explaine">Next.js / SCSS / StaticForm / Docker</span></p>
                 </div>
                 <div className="work-img">
                     <a href="https://tokyo-share.herokuapp.com/" target="_blank"><img src={tokyoshare} alt="tokyoshare" /></a>
-                    <p className="title">Share house seach service in Tokyo<br /><span className="skill-explaine">HTML / SCSS / PHP / Laravel / PostgreSQL / Docker</span></p>
+                    <p className="title">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="en" element={<EnShareHouseDescribe />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <br /><span className="skill-explaine">HTML / SCSS / PHP / Laravel / PostgreSQL / Docker</span></p>
                 </div>
                 <div className="work-img">
                     <a href="https://unit-price-calculator.netlify.app/" target="_blank"><img src={calc} alt="unit price calculator" /></a>
-                    <p className="title">Unit price calculator<br /><span className="skill-explaine">Vue.js / SCSS</span></p>
+                    <p className="title">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="en" element={<EnUnitPriceCalcDescribe />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <br /><span className="skill-explaine">Vue.js / SCSS</span></p>
                 </div>
                 <div className="work-img">
                     <a href={isara_png} target="_blank"><img src={isara} alt="isaraサイト模写" /></a>
-                    <p className="title">iSara LP practice(<a href="https://isara.life/" target="_blank">original web site</a>)<br /><span className="skill-explaine">HTML / SCSS</span></p>
+                    <p className="title">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="en" element={<EnIsaraDescribe />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <br /><span className="skill-explaine">HTML / SCSS</span></p>
                 </div>
                 <div className="work-img">
                     <a href="https://aklog.net/" target="_blank"><img src={aklog} alt="blog" /></a>
-                    <p className="title">My blog<br /><span className="skill-explaine">PHP / WordPress</span></p>
+                    <p className="title">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="en" element={<EnBlogDescribe />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <br /><span className="skill-explaine">PHP / WordPress</span></p>
                 </div>
                 <div className="work-img">
                     <a href=""><img src={portfolio} alt="tokyosyare" /></a>
-                    <p className="title">My portfolio<br /><span className="skill-explaine">React.js / SCSS</span></p>
+                    <p className="title">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="en" element={<EnPortfolioDescribe />} />
+                            </Routes>
+                        </BrowserRouter>
+                        <br /><span className="skill-explaine">React.js / SCSS</span></p>
                 </div>
             </div>
 
